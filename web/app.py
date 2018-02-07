1b27366a95f81cd0b6a8e8fb178d52d900b60193
Duncan Parkes <deparkes@gmail.com>
2018-02-07 21:49:41 +0000

    Back to working

--- web/app.py
+++ web/app.py
@@ -1,12 +1,19 @@
-#!flask/bin/python
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello, World!"
+def hello_world():
+    return 'Flask Dockerized'
 
+@app.route('/test', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
+    
+    
+
+
+
+
