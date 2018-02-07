86cb74182fada77f9678a4063655f9c7e2cf1aaf
Duncan Parkes <deparkes@gmail.com>
2018-02-07 22:19:57 +0000

    Create working simple api.
    Needed to follow some workarounds to use json.dumps(...) rather than
    jsonify(...) as jsonify does not work with lists it seems.
    
    Following the flask api guide on programming historian

--- web/app.py
+++ web/app.py
@@ -1,5 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify, Response
+import json
 app = Flask(__name__)
+
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
 
 @app.route('/')
 def hello_world():
@@ -8,7 +27,13 @@ def hello_world():
 @app.route('/test', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
-    
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return Response(json.dumps(books),  mimetype='application/json')
+  
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
     
