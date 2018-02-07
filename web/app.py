fed78bb5495e33aa0e5286ff0a9ed96c95a31579
Duncan Parkes <deparkes@gmail.com>
2018-02-07 21:40:40 +0000

    Whatever we have here

--- web/app.py
+++ web/app.py
@@ -1,9 +1,12 @@
+#!flask/bin/python
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
+def index():
+    return "Hello, World!"
 
+    
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
