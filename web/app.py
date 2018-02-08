b26d0d24f1eaa32e4359a733722f22d1d9e54f35
Duncan Parkes <deparkes@gmail.com>
2018-02-08 20:05:37 +0000

    Change to sqlite db

--- web/app.py
+++ web/app.py
@@ -1,58 +1,65 @@
 from flask import Flask, request, jsonify, Response
 import json
+import sqlite3 as sqlite
 app = Flask(__name__)
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
 
-@app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
 
-@app.route('/test', methods=['GET'])
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+    
+@app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return """<h1>Distant Reading Archive</h1>
+    <p>A prototype API for distant reading of science fiction novels</p>
+    """
 
 # A route to return all of the available entries in our catalog.
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
-    return jsonify(books)
+    conn = sqlite.connect('../data/books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_books = cur.execute("SELECT * FROM books;").fetchall()
+    return jsonify(all_books)
+
+@app.route("/api/v1/resources/books", methods=['GET'])
+def api_filter():
+    query_parameters = request.args
     
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
-
-    # Create an empty list for our results
-    results = []
-
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
- 
-    return Response(json.dumps(results),  mimetype='application/json')
+    id = query_parameters.get('id')
+    published = query_parameters.get('published')
+    author = query_parameters.get('author')
+    
+    
+    query = "SELECT * FROM books WHERE"
+    to_filter = []
+    
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if published:
+        query += ' published=? AND'
+        to_filter.append(published)
+    if author:
+        query += ' author=? AND'
+        to_filter.append(author)
+    if not (id or published or author):
+        return page_not_found(404)
+    
+    query = query[:-4] + ';'
+    
+    conn = sqlite.connect('../data/books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    
+    results = cur.execute(query, to_filter).fetchall()
+    
+    return jsonify(results)
+    
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
