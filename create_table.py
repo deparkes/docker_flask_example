56a946396d24bd5f20ce14c4c046dfb559495533
Duncan Parkes <deparkes@gmail.com>
2018-02-11 15:29:42 +0000

    Remove unneeded create_table

--- create_table.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('./data/database.db')
-print("Opened database successfully")
-
-conn.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
-print("Table created successfully")
-conn.close()
