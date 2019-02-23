e89720ea181fd4f23ab40bdaa001f408a3dfeb94
Duncan Parkes <deparkes@gmail.com>
2019-02-23 14:52:49 +0000

    Refactor build_select_books_query

--- api/app.py
+++ api/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, jsonify, Response
-import json
+from flask import Flask, request, jsonify, render_template
 import sqlite3 as sqlite
 app = Flask(__name__)
 
@@ -17,6 +16,10 @@ def home():
     <p>A prototype API for distant reading of science fiction novels</p>
     """
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 # A route to return all of the available entries in our catalog.
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
@@ -33,11 +36,40 @@ def api_filter():
     id = query_parameters.get('id')
     published = query_parameters.get('published')
     author = query_parameters.get('author')
-    
-    
-    query = "SELECT * FROM books WHERE"
+
     to_filter = []
+    query = build_select_books_query(author, id, published, to_filter)
+    conn = sqlite.connect('../data/books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
     
+    results = cur.execute(query, to_filter).fetchall()
+    
+    return jsonify(results)
+
+@app.route("/api/v1/resources/books/json", methods=['GET'])
+def api_filter_json():
+    books = request.get_json()
+    results = []
+    for book in books['books']:
+        to_filter = []
+
+        id = book['id']
+        published = book['published']
+        author = book['author']
+        query = build_select_books_query(author, id, published, to_filter)
+
+        conn = sqlite.connect('../data/books.db')
+        conn.row_factory = dict_factory
+        cur = conn.cursor()
+
+        results.append(cur.execute(query, to_filter).fetchall()[0])
+
+    return jsonify(results)
+
+
+def build_select_books_query(author, id, published, to_filter):
+    query = "SELECT * FROM books WHERE"
     if id:
         query += ' id=? AND'
         to_filter.append(id)
@@ -48,55 +80,14 @@ def api_filter():
         query += ' author=? AND'
         to_filter.append(author)
     if not (id or published or author):
-        return page_not_found(404)
-    
+        pass
+        # return page_not_found(404)
     query = query[:-4] + ';'
-    
-    conn = sqlite.connect('../data/books.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-    
-    results = cur.execute(query, to_filter).fetchall()
-    
-    return jsonify(results)
+    return query
 
-@app.route("/api/v1/resources/books/json", methods=['POST'])
-def api_filter_json():
-    books = request.get_json()
-    results = []
-    for book in books['books']:
-        query = "SELECT * FROM books WHERE"
-        to_filter = []
 
-        id = book['id']
-        published = book['published']
-        author = book['author']
-
-        if id:
-            query += ' id=? AND'
-            to_filter.append(id)
-        if published:
-            query += ' published=? AND'
-            to_filter.append(published)
-        if author:
-            query += ' author=? AND'
-            to_filter.append(author)
-        if not (id or published or author):
-            return page_not_found(404)
-
-        query = query[:-4] + ';'
-
-        conn = sqlite.connect('../data/books.db')
-        conn.row_factory = dict_factory
-        cur = conn.cursor()
-
-        results.append(cur.execute(query, to_filter).fetchall()[0])
-
-    return jsonify(results)
-
-    
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
     
     
 
